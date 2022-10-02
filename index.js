fd1d310758ca224a666babdada242c2700403b50
steveen de la rosa <steev@MacBook-Pro-de-steveen.local>
2022-10-02 11:59:27 -0500

    connection mysql, register

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import http from "http";
 import bodyParser from "body-parser";
-
+import "dotenv/config.js";
 //routes
 import oauth from "./src/components/oauth/main.js";
 
