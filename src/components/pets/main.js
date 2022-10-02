2bc968bc864ab038627f457e19e8f2f2aa616d6c
sebastianocamporoa <sebastian45roa@gmail.com>
2022-10-02 17:23:44 -0500

    [Update] create endpoint to add pets

--- src/components/pets/main.js
+++ src/components/pets/main.js
@@ -1,8 +1,10 @@
-import { express } from "express";
+import express from "express";
 import controller from "./controller.js";
 
 const pets = express.Router();
 
 pets.get("/", controller.getPets);
+
+pets.post("/", controller.postPets);
 
 export default pets;
