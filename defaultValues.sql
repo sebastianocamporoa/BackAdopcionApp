be9766573f274badd73dbd4ec7c69946df6a13f0
sebastianocamporoa <sebastian45roa@gmail.com>
2022-10-16 15:01:27 -0500

    update values

--- defaultValues.sql
+++ defaultValues.sql
@@ -17,10 +17,27 @@ INSERT INTO mydb.cities (name, country_id) values
 ('Medellín', 1),
 ('Cali', 1);
 
-INSERT INTO mydb.localities (name, city_id) values 
-('Martires', 1),
-('Usme', 1),
-('Kennedy', 1);
+INSERT INTO locality (id, name, cities_id) VALUES
+(1,'Usaquen',1),
+(2,'Chapinero',1),
+(3,'Santa Fe',1),
+(4,'San Cristobal',1),
+(5,'Usme',1),
+(6,'Tunjuelito',1),
+(7,'Bosa',1),
+(8,'Kennedy',1),
+(9,'Fontibon',1),
+(10,'Engativa',1),
+(11,'Suba',1),
+(12,'Barrios Unidos',1),
+(13,'Teusaquillo',1),
+(14,'Los Martires',1),
+(15,'Antinio Nariño',1),
+(16,'Puente Aranda',1),
+(17,'La Candelaria',1),
+(18,'Rafael Uribe Uribe',1),
+(19,'Ciudad Bolivar',1),
+(20,'Sumapaz',1);
 
 INSERT INTO mydb.pet_types (name) values 
 ('Perro'), ('Gato');
